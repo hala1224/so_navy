f408ce12ea8cdcb4ba46030472486faf0eed4c2a
Natalie Halawani <hala1224@gmail.com>
2019-02-01 21:05:06 +0000

    First scrape

--- lib/so_navy/scraper.rb
+++ lib/so_navy/scraper.rb
@@ -3,6 +3,9 @@ class SoNavy::Scraper
   
   def self.scrape_categories(url)
     
-    puts "Made it to scraper"
+    # puts "Made it to scraper"
+    webpage = Nokogiri::HTML(open(url))
+    # webpage.css("div.content  h2  a")[3].text
+    binding.pry
   end
 end
