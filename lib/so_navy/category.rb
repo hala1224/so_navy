7ba2c08d44fdf528950c44c98d787ce5bbb9d797
Natalie Halawani <hala1224@gmail.com>
2019-02-11 01:05:10 +0000

    adjusted CLI for different part of site to scrape

--- lib/so_navy/category.rb
+++ lib/so_navy/category.rb
@@ -1,22 +1,26 @@
 class SoNavy::Category
-  attr_accessor :name, :url
+  attr_accessor :name, :url, :type
   attr_reader :items
     
     
   @@all=[]
-  def initialize(name, url)
+  def initialize(name, url, type)
     @name=name
-    @url=url
-      # @url="https://somethingnavy.com/" + url
+    # @url=url
+    @url="https://somethingnavy.com/shop" + url
     @items=[]
-    @@all << self    
+    @@all << self    #saving or remembering the object
   end  
   
-  
-  
+
   def self.all
     @@all
   end
+
+  def self.type(type)
+    @@all.select{|category| category.type == type}
+  end
+
   
   def add_item(item)
     self.items << item
--- lib/so_navy/cli.rb
+++ lib/so_navy/cli.rb
@@ -1,53 +1,69 @@
 class SoNavy::CLI 
   
+  attr_reader :type
   
   def start 
-    puts "\n \n      Welcome to so_navy!" #instance method
+    puts "\n \n      Welcome to so_navy!".colorize(:yellow) #instance method
     menu
   end
   
   def menu
       
-    puts "\n Which category would you like to see?"
+    puts "\n Which category would you like to shop?"
     puts ""
-    puts "1 - Something Navy \n2 - Fashion \n3 - Something else \n4 - to Exit"
+    puts "1".colorize(:yellow)  +" - Designer clothing"
+    puts "\n2".colorize(:yellow)+" - Designer Accesories"
+    puts "\n3".colorize(:yellow)+" - Beauty"
+    puts "\n4".colorize(:yellow)+" - to Exit"
+    puts ""
     input = gets.strip.to_i 
     case input
        when 1
-         puts "You chose Something Navy"
-        # Scrapping the Something Navy category
-        # url = "https://somethingnavy.com/category/something-navy/"
-        # categories = SoNavy::Scraper.scrape_categories(url)
+         puts "\n            You chose Designer Clothing".colorize(:blue)
+         puts ""
+  
+        # Scrapping Designer Clothing category
         
-        # scrape Something Navy category
-        scrape_somenavy
+        @type = "clothes"
+        if Dealio::Category.type(@type) == []
+          scrape_categories
+        end
         list_categories
         choose_category
-        
-        # puts categories[0].name
-        # puts categories[0].url
+      
    
-   e    when 2
-          puts "You chose Fashion"
-          # scrape Fashion category
-          scrape_fashion
-          list_categories
-          choose_category
-          
+     when 2
+         puts "\n            You chose Designer Accesories".colorize(:blue)
+         puts ""
+         # Scrapping Designer Accesories category
+        
+        @type = "accessories"
+        if Dealio::Category.type(@type) == []
+          scrape_categories
+        end
+        list_categories
+        choose_category
+
           
        when 3
-          puts "You chose Something else"
-          
-          scrape_somelse
-          list_categories
-          choose_category
-          
+         puts "\n            You chose Beauty".colorize(:blue)
+         puts ""
+         # Scrapping Beauty category
+        
+        @type = "beauty"
+        if Dealio::Category.type(@type) == []
+          scrape_categories
+        end
+        list_categories
+        choose_category
           
        when 4
           puts "Thanks for stopping by!"
     else 
       
-      puts "Please enter numbers 1 through 4"
+      puts "\nPlease enter numbers 1 through 4".colorize(:red)
+      menu # recursion
+      
     end
   end 
   
@@ -57,60 +73,62 @@ class SoNavy::CLI
      end
   end
   
-  def choose_category
-    puts "\nChoose a category by selecting a number:"
-    input = gets.strip.to_i
-    max_value = SoNavy::Category.all.length
-    if input.between?(1,max_value)
   
-      category = SoNavy::Category.all[input-1]
-      puts "In choose_category"
+  def choose_category
+    puts "\nChoose a category by selecting a number:".colorize(:light_blue)
+    input = gets.strip.to_i
+    max_value = SoNavy::Category.type(@type).length
+    if input.between?(1,max_value)
+      category = SoNavy::Category.type(@type)[input-1]
       display_category_items(category)
     else
-      puts "\nPlease put in a valid input"
+      puts "\nPlease put in a valid input".colorize(:red)
       list_categories
       choose_category
     end
-    
-   
   end
   
   
-   def display_category_items(category)
+  
+  def display_category_items(category)
      
-    # puts "In display and category is" 
-    # puts category.url
-     SoNavy::Scraper.scrape_items(category)
-     puts "Here are the items for #{category.name}:\n"
+    # Making sure to scrape only once
      
-    puts " In display_category length #{category.items.length}"
-     category.items.each.with_index(1) do |item, index|   #represents an array of item objects
-        #print out info about each item
-        puts "\n#{index}. #{item.product}"
-        puts item.description
+     if category.description == [] 
+        SoNavy::Scraper.scrape_items(category)
+     end  
+     
+     if category.items == []
+       puts "Sorry no inventory!"
+     else
+     
+       puts "\n       Here are the items for ".colorize(:red)+"#{category.name}:\n".colorize(:blue)
+       category.items.each.with_index(1) do |item, index|   #represents an array of item objects
+          #print out info about each item
+          puts "\n#{index}. #{item.product}".colorize(:light_blue)
+          puts item.description
+      end
+      puts "\n       That's all for this weeks'  ".colorize(:yellow)+"#{category.name}!\n".colorize(:light_blue)
     end
-    second_menu
+     second_menu
   end
   
-   def scrape_somenavy
-      url = "https://somethingnavy.com/category/something-navy/"
-      categories =  SoNavy::Scraper.scrape_categories(url)
+
+  
+  def scrape_categories
+      categories =  SoNavy::Scraper.scrape_categories(@type)
   end
   
-  def scrape_fashion
-     url="https://somethingnavy.com/category/fashion/"
-     categories =  SoNavy::Scraper.scrape_categories(url)
-  end
   
-  def scrape_somelse
-    url="https://somethingnavy.com/category/something-else/"
-    categories =  SoNavy::Scraper.scrape_categories(url)
-  end
+
   
   def second_menu
-    puts "Would you like to look at another category? Type 'C'"
-    puts "Would you like to go to the start? Type 'S'"
-    puts "Would you like to exit? Type 'E'"
+    puts "\n       Would you like to:".colorize(:red)
+    puts""
+    puts "'C'".colorize(:blue) + " - look at another " + "Category?".colorize(:light_blue)
+    puts "'S'".colorize(:blue) + " - go back to  " +"Start?".colorize(:light_blue)
+    puts "'E'".colorize(:blue) + " - Exit? ".colorize(:light_blue)
+    puts ""
     input = gets.strip.upcase
     if input == "C"
       list_categories
@@ -118,7 +136,9 @@ class SoNavy::CLI
     elsif input == "S"
       menu
     elsif input == "E"
-      puts "Goodbye!"
+    
+      puts "\nThanks for shopping with "+"So_Navy!".colorize(:light_blue)
+      puts "           Goodbye!".colorize(:yellow)
     else
       puts "Sorry I couldn't understand that command"
       second_menu
